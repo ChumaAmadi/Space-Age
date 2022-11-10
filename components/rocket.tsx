7511442f4283b6c77a3bc7fafa22e2b480b1ea94
Chuma <C.NnodimAmadi001@umb.edu>
2022-11-10 13:28:41 -0500

    Added cursor pointer to rocket & about page

--- components/rocket.tsx
+++ components/rocket.tsx
@@ -1,11 +1,12 @@
 import styles from '../styles/Home.module.css'
 import Image from 'next/image'
 import rocket from '../public/rocket.png'
+import Link from 'next/link'
 
 export default function Rocket() {
   return (
     <div className="rocketClass">
-        <Image src={rocket} alt="rocket" className={styles.rocket} />
+        <Link href='/about'><Image src={rocket} alt="rocket" className={styles.rocket} /></Link>
     </div>
   )
 }

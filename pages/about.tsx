69de1f6f5741271c4e8142d279b4d70f2973da11
Chuma <C.NnodimAmadi001@umb.edu>
2022-11-17 02:04:25 -0500

    Needs Icons

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,21 +5,24 @@ import Link from 'next/link'
 export default function About() {
   return (
     <div>
-        <h3 className={styles.whatIs}>What Is Space Age?</h3>
-        <div className={styles.aboutContainer}>
-            <p className={styles.aboutText}>
-                Space Age is click-based game where your choices matter & follow you through the stars
-            </p>
-            <p className={styles.aboutText1}>
-                You can choose to be a merchant, pirate, or an explorer(More Coming Soon)
-            </p>
-            <p className={styles.aboutText2}>
-                Decide your path & create your own story
-            </p>
-        <h3 className={styles.rulesOverview}>Rules</h3>
-        <p className={styles.rulesText}>Click on any of the icons to read about that aspect of the game</p>
-        <div className={styles.rulesContainer}>
-        </div>
+      <video autoPlay muted loop className={styles.video}>         
+        <source src="/star.mp4" type="video/mp4"/>       
+      </video>
+      <h3 className={styles.question}>What Is Space Age?</h3>
+      <div className={styles.aboutContainer}>
+         <p className={styles.aboutText}>
+            Space Age is a click-based game where your choices truly matter.
+        </p>
+        <p className={styles.aboutText1}>
+          You can choose to be a merchant, pirate, or an explorer.
+        </p>
+        <p className={styles.aboutText2}>
+          Decide your path & create your own story.
+        </p>
+      </div>
+      <h3 className={styles.rulesOverview}>Rules</h3>
+      <p className={styles.rulesText}>Click on any of the icons to read about that aspect of the game</p>
+      <div className={styles.rulesContainer}>
       </div>
     </div>
   )

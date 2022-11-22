d406e8e229df0212e4f289e1d8b47c86a55d1cdf
Chuma <C.NnodimAmadi001@umb.edu>
2022-11-22 13:50:55 -0500

    Added Fade Animations

--- components/icon-slideshow.tsx
+++ components/icon-slideshow.tsx
@@ -8,11 +8,9 @@ import explorer from '../public/rocket-icon.jpg'
 export default function IconSlideshow() {
   return (
     <div className={styles.slideshowContainer}>
-      <div className={styles.mySlidesHidden}>
-          <Image src={pirate} alt="pirate" width={100} height={100} />
-          <Image src={merchant} alt="merchant" width={100} height={100} />
-          <Image src={explorer} alt="explorer" width={100} height={100} />
-      </div>
+        <Image src={pirate} alt="pirate" width={100} height={100} className={styles.myslidesHidden} />
+        <Image src={merchant} alt="merchant" width={100} height={100} className={styles.myslidesHidden} />
+        <Image src={explorer} alt="explorer" width={100} height={100} className={styles.myslidesHidden} />
     </div>
   )
 }

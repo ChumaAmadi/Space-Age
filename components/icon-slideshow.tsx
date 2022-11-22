43ba4ccabdf2285da1f2b4ee5d6a406fc74786ef
Chuma <C.NnodimAmadi001@umb.edu>
2022-11-22 16:37:49 -0500

    Removed Slideshow

--- components/icon-slideshow.tsx
+++ components/icon-slideshow.tsx
@@ -3,14 +3,3 @@ import Image from 'next/image'
 import pirate from '../public/pirate.png'
 import merchant from '../public/merchant.png'
 import explorer from '../public/rocket-icon.jpg'
-
-
-export default function IconSlideshow() {
-  return (
-    <div className={styles.slideshowContainer}>
-        <Image src={pirate} alt="pirate" width={100} height={100} className={styles.myslidesHidden} />
-        <Image src={merchant} alt="merchant" width={100} height={100} className={styles.myslidesHidden} />
-        <Image src={explorer} alt="explorer" width={100} height={100} className={styles.myslidesHidden} />
-    </div>
-  )
-}

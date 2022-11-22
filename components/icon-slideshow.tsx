f0a039b6b6139563d28aa7e099d70552664680cd
Chuma <C.NnodimAmadi001@umb.edu>
2022-11-22 16:39:35 -0500

    Slideshow Re-try

--- components/icon-slideshow.tsx
+++ components/icon-slideshow.tsx
@@ -3,3 +3,11 @@ import Image from 'next/image'
 import pirate from '../public/pirate.png'
 import merchant from '../public/merchant.png'
 import explorer from '../public/rocket-icon.jpg'
+
+export default function IconSlideshow() {
+  return (
+    <div className={styles.iconSlideshow}>
+      
+    </div>
+  );
+}

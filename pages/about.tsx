9ad5c66b94391fa2eed92c644e731b99fcae9047
Chuma <C.NnodimAmadi001@umb.edu>
2022-11-28 00:00:23 -0500

    UI Changes

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,9 @@
 import styles from '../styles/Home.module.css'
 import Image from 'next/image'
 import Link from 'next/link'
-import Icons from '../components/icons'
+import pirate from '../public/pirate.png'
+import merchant from '../public/merchant.png'
+import explorer from '../public/space.png'
 
 export default function About() {
   return (
@@ -11,17 +13,20 @@ export default function About() {
       </video>
       <h3 className={styles.question}>What Is Space Age?</h3>
       <div className={styles.aboutContainer}>
-        <Icons />
+        <Image src={pirate} alt="Pirate" className={styles.pirate} /> 
         <p className={styles.aboutText}>
-            In Space Age, your choices truly matter.
+          In Space Age, your choices truly matter.
         </p>
+        <Image src={merchant} alt="Merchant" className={styles.merchant} />
         <p className={styles.aboutText1}>
           You can choose to be a merchant, pirate, or an explorer.
         </p>
+        <Image src={explorer} alt="Explorer" className={styles.explorer} />
         <p className={styles.aboutText2}>
           Decide your path & create your own story.
         </p>
       </div>
+      
       <h3 className={styles.rulesOverview}>Rules</h3>
       <p className={styles.rulesText}>Click on any of the icons to read about that aspect of the game</p>
       <div className={styles.rulesContainer}>

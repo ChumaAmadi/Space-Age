cb62f75ecb02f12b7ac5ebd7b2aec2d7b797207e
chuma <C.NnodimAmadi001@umb.edu>
2023-03-11 16:44:58 -0500

    Storage Management + Footer Work

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,13 +1,28 @@
 import styles from '../styles/Footer.module.css'
+import Image from 'next/image'
+import Twitter from '../public/twitter.png'
+import Discord from '../public/discord.png'
+import Magic from '../public/magic-eden.png'
 
 export default function Footer() {
     return (
         <div className={styles.mainContainer}>
-            <h3 className={styles.footerText}>Enter The Stars</h3>
-            <div className={styles.footerContainer}>
-                <button className={styles.footerButton}>Discord</button>
-                <button className={styles.footerButton}>Twitter</button>
-                <button className={styles.footerButton}>Magic Eden</button>
+            <div className={styles.footerLeftContainer}>
+                <h3 className={styles.footerHeader}>Space Age</h3>
+                <p className={styles.footerText}>© 2021 Space Age. All Rights Reserved.</p>
+                <h3 className={styles.solanaHeader}>Built On The Solana Ecosystem</h3>
+            </div>
+
+            <div className={styles.footerRightContainer}>
+                <div className={styles.img1}>
+                    <Image src={Twitter} alt=" " className={styles.twitter} />
+                </div>
+                <div className={styles.img2}>
+                    <Image src={Discord} alt=" " className={styles.discord} />
+                </div>
+                <div className={styles.img3}>
+                    <Image src={Magic} alt=" " className={styles.magic} />
+                </div>
             </div>
         </div>
     )
